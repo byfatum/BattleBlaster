95d77f6548166665c7936ca9ae40b3334b40add6
dymaiosvoem <dymaiosvoem@yandex.ru>
2026-06-23 22:51:28 +0300

    Use GameMode to spawn Tank pawn

---
+++ Source/BattleBlaster/BattleBlasterGameMode.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+
+#include "BattleBlasterGameMode.generated.h"
+
+UCLASS()
+class BATTLEBLASTER_API ABattleBlasterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
