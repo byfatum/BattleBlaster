9bdfab87a287cd6db97facee4671fd04cdb00968
dymaiosvoem <dymaiosvoem@yandex.ru>
2026-07-09 19:57:06 +0300

    Implement projectile actor

---
+++ Source/BattleBlaster/Projectile.cpp
@@ -0,0 +1,43 @@
+#include "Projectile.h"
+#include "BattleBlasterCollision.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/EngineTypes.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+AProjectile::AProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ProjectileComponent"));
+	SetRootComponent(ProjectileMesh);
+	
+	ProjectileMesh->SetGenerateOverlapEvents(true);
+	ProjectileMesh->SetCollisionProfileName(TEXT("Projectile"));
+	
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+	ProjectileMovementComponent->InitialSpeed = 1000.0f;
+	ProjectileMovementComponent->MaxSpeed = 1000.0f;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+	ProjectileMovementComponent->bShouldBounce = false;
+}
+
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	ProjectileMesh->OnComponentHit.AddDynamic(this, &AProjectile::OnComponentHit);
+}
+
+void AProjectile::OnComponentHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (OtherActor)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Projectile hit: %s"), *OtherActor->GetActorNameOrLabel());
+	}
+}
---
+++ Source/BattleBlaster/Projectile.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+class UStaticMeshComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class BATTLEBLASTER_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AProjectile();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UFUNCTION()
+	void OnComponentHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Visual")
+	TObjectPtr<UStaticMeshComponent> ProjectileMesh;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Movement")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+};
