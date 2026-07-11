bb140a7369bb02927ab67ee1b7a507f36b33c4d8
dymaiosvoem <dymaiosvoem@yandex.ru>
2026-07-11 14:19:19 +0300

    Remove temporary projectile debug logs

--- Source/BattleBlaster/Projectile.cpp
+++ Source/BattleBlaster/Projectile.cpp
@@ -37,18 +37,12 @@ void AProjectile::BeginPlay()
 void AProjectile::OnComponentHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, 
 		UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
-	UE_LOG(LogTemp, Warning, TEXT("OnComponentHit called"));
-	
 	const AActor* const ProjectileOwner = this->GetOwner();
-	UE_LOG(LogTemp, Error, TEXT("ProjectileOwner is %s"), ProjectileOwner ? TEXT("Valid") : TEXT("nullptr"));
 	
 	ABasePawn* const DamagedActor = Cast<ABasePawn>(OtherActor);
-	UE_LOG(LogTemp, Error, TEXT("DamagedActor is %s"), DamagedActor ? TEXT("Valid") : TEXT("nullptr"));
 	
 	if (DamagedActor && ProjectileOwner && ProjectileOwner != OtherActor)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Base Pawn %s take damage"), *DamagedActor->GetActorNameOrLabel());
-		
 		UGameplayStatics::ApplyDamage(
 			DamagedActor, 
 			ProjectileDamage, 
