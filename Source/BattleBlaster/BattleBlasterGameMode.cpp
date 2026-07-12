b5827e1c618613470a46db71bba294c95a8d38c5
dymaiosvoem <dymaiosvoem@yandex.ru>
2026-07-12 18:24:57 +0300

    Implement pawn registration and death handling, check victory and defeat conditions

--- Source/BattleBlaster/BattleBlasterGameMode.cpp
+++ Source/BattleBlaster/BattleBlasterGameMode.cpp
@@ -1,1 +1,34 @@
 ﻿#include "BattleBlasterGameMode.h"
+#include "BasePawn.h"
+#include "Tank.h"
+#include "Tower.h"
+
+void ABattleBlasterGameMode::RegisterPawn(ABasePawn* const Pawn)
+{
+	if (ATower* const Tower = Cast<ATower>(Pawn))
+	{
+		AliveTowers.Add(Tower);
+	}
+}
+
+void ABattleBlasterGameMode::ActorDied(ABasePawn* const Pawn)
+{
+	if (ATower* const Tower = Cast<ATower>(Pawn))
+	{
+		AliveTowers.Remove(Tower);
+		
+		if (AllTowersDead())
+		{
+			UE_LOG(LogTemp, Display, TEXT("You Victory! All Towers are dead"));
+		}
+	}
+	else if (ATank* const Tank = Cast<ATank>(Pawn))
+	{
+		UE_LOG(LogTemp, Display, TEXT("You Lose! Try Again"));
+	}
+}
+
+bool ABattleBlasterGameMode::AllTowersDead() const
+{
+	return AliveTowers.Num() == 0;
+}
--- Source/BattleBlaster/BattleBlasterGameMode.h
+++ Source/BattleBlaster/BattleBlasterGameMode.h
@@ -2,12 +2,24 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Containers/Set.h"
 
 #include "BattleBlasterGameMode.generated.h"
+
+class ABasePawn;
+class ATower;
 
 UCLASS()
 class BATTLEBLASTER_API ABattleBlasterGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+public:
+	void RegisterPawn(ABasePawn* const Pawn);
+	void ActorDied(ABasePawn* const Pawn);
+	
+private:
+	bool AllTowersDead() const;
+	
+	TSet<TWeakObjectPtr<ATower>> AliveTowers;
 };
