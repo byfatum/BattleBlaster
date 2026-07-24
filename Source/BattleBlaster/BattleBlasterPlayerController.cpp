9c5be4d34854a583117e1458d101030394b16e6e
dymaiosvoem <dymaiosvoem@yandex.ru>
2026-07-24 19:42:22 +0300

    Implement screen messages in PlayerController

--- Source/BattleBlaster/BattleBlasterPlayerController.cpp
+++ Source/BattleBlaster/BattleBlasterPlayerController.cpp
@@ -1,7 +1,82 @@
 #include "BattleBlasterPlayerController.h"
+#include "BattleBlasterGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "ScreenMessage.h"
+
+#define LOCTEXT_NAMESPACE "BattleBlasterPlayerController"
 
 ABattleBlasterPlayerController::ABattleBlasterPlayerController()
 {
 	bShowMouseCursor = true;
 	DefaultMouseCursor = EMouseCursor::Crosshairs;
 }
+
+void ABattleBlasterPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	GameMode = Cast<ABattleBlasterGameMode>(UGameplayStatics::GetGameMode(this));
+	if (GameMode.Get())
+	{
+		ScreenMessageObject = CreateWidget<UScreenMessage>(this, ScreenMessageClass);
+		if (!ScreenMessageObject) return;
+		
+		ScreenMessageObject->AddToPlayerScreen();
+		ScreenMessageObject->SetMessageText(LOCTEXT("GameplayStarting", "Get Ready!"));
+		
+		GameMode->OnGameplayEnabledChanged().AddUObject(this, &ABattleBlasterPlayerController::HandleGameplayEnabledChanged);
+		GameMode->OnGameResultChanged().AddUObject(this, &ABattleBlasterPlayerController::HandleGameResultChanged);
+		GameMode->OnCountDownDelayChanged().AddUObject(this, &ABattleBlasterPlayerController::HandleRemainingCountDownSeconds);
+	}
+}
+
+void ABattleBlasterPlayerController::HandleGameplayEnabledChanged()
+{
+	FTimerHandle HideScreenMessage;
+	GetWorldTimerManager().SetTimer(
+		HideScreenMessage,
+		this, 
+		&ABattleBlasterPlayerController::ApplyGameplayEnabledChanged, 
+		1.0f, 
+		false
+	);
+}
+
+void ABattleBlasterPlayerController::HandleGameResultChanged()
+{
+	ApplyGameResultChanged(GameMode->GetGameResult());
+}
+
+void ABattleBlasterPlayerController::HandleRemainingCountDownSeconds()
+{
+	ApplyRemainingCountDownSeconds(GameMode->GetRemainingCountDownSeconds());
+}
+
+void ABattleBlasterPlayerController::ApplyGameplayEnabledChanged()
+{
+	ScreenMessageObject->SetVisibility(
+		GameMode->IsGameplayEnabled() && GameMode->GetGameResult() == EGameResult::InProgress ? 
+		ESlateVisibility::Collapsed : ESlateVisibility::Visible
+	);
+}
+
+void ABattleBlasterPlayerController::ApplyGameResultChanged(EGameResult GameResult)
+{
+	ScreenMessageObject->SetMessageText(
+		GameResult == EGameResult::Victory ? 
+		LOCTEXT("GameResultVictory", "Victory!") : 
+		LOCTEXT("GameResultDefeat", "Defeat!")
+	);
+	
+	ScreenMessageObject->SetVisibility(ESlateVisibility::Visible);
+}
+
+void ABattleBlasterPlayerController::ApplyRemainingCountDownSeconds(float RemainingCountDownSeconds)
+{
+	ScreenMessageObject->SetMessageText(
+		RemainingCountDownSeconds <= 0 ? 
+		LOCTEXT("CountDownTimerTimeoutOver", "Go!") : FText::AsNumber(RemainingCountDownSeconds)
+	);
+}
+
+#undef LOCTEXT_NAMESPACE
--- Source/BattleBlaster/BattleBlasterPlayerController.h
+++ Source/BattleBlaster/BattleBlasterPlayerController.h
@@ -4,6 +4,10 @@
 #include "GameFramework/PlayerController.h"
 #include "BattleBlasterPlayerController.generated.h"
 
+class ABattleBlasterGameMode;
+class UScreenMessage;
+enum class EGameResult : uint8;
+
 UCLASS()
 class BATTLEBLASTER_API ABattleBlasterPlayerController : public APlayerController
 {
@@ -11,4 +15,25 @@ class BATTLEBLASTER_API ABattleBlasterPlayerController : public APlayerControlle
 	
 public:
 	ABattleBlasterPlayerController();
+	
+protected:
+	virtual void BeginPlay() override;
+	
+	void HandleGameplayEnabledChanged();
+	void HandleGameResultChanged();
+	void HandleRemainingCountDownSeconds();
+	
+	void ApplyGameplayEnabledChanged();
+	void ApplyGameResultChanged(EGameResult GameResult);
+	void ApplyRemainingCountDownSeconds(float RemainingCountDownSeconds);
+	
+private:
+	UPROPERTY(VisibleAnywhere)
+	TWeakObjectPtr<ABattleBlasterGameMode> GameMode;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UScreenMessage> ScreenMessageClass;
+	
+	UPROPERTY(Transient)
+	TObjectPtr<UScreenMessage> ScreenMessageObject;
 };
